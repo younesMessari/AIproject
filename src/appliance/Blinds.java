96d7417307239a35a2f52d4244164842b74a1799
Younes Messari <73780929+younesMessari@users.noreply.github.com>
2020-12-12 18:02:43 +0100

    Add files via upload

---
+++ src/appliance/Blinds.java
@@ -0,0 +1,4 @@
+package appliance;
+
+public class Blinds {
+}
---
+++ src/house/House.java
@@ -0,0 +1,8 @@
+package house;
+import java.util.ArrayList;
+import appliance.Alarm;
+
+public class House {
+    private ArrayList<Room> rooms;
+    private Alarm alarm;
+}
