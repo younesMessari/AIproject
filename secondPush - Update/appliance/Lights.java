003a71e489265316509d4fa85ff2fbc3c0e29509
SlickGrove <68993978+SlickGrove@users.noreply.github.com>
2020-12-16 10:37:12 +0100

    Update Lights.java

--- secondPush - Update/appliance/Lights.java
+++ secondPush - Update/appliance/Lights.java
@@ -1,4 +1,100 @@
 package appliance;
 
 public class Lights {
+    
+    private double CurrentLux;
+    private double DesiredLux;
+    private boolean Switch;
+
+    public double getCurrentLux() {
+        return CurrentLux;
+    }
+
+    public void setCurrentLux(double CurrentLux) {
+        this.CurrentLux = CurrentLux;
+    }
+
+    public double getDesiredLux() {
+        return DesiredLux;
+    }
+
+    public void setDesiredLux(double DesiredLux) {
+        this.DesiredLux = DesiredLux;
+    }
+
+    public boolean isSwitch() {
+        return Switch;
+    }
+
+    public void setSwitch(boolean Switch) {
+        this.Switch = Switch;
+    }
+    
+   
+    public boolean isOn(){
+        if (this.Switch == true)
+                return true;
+        else{
+                return false;}
+    }
+    
+    public boolean isOff(){
+        if (this.Switch == false)
+                return true;
+        else {
+            return false;}
+    }
+    
+    public boolean Brighten(){
+        if (isOn()==true){
+            if (CurrentLux<DesiredLux){
+                while(CurrentLux>DesiredLux){
+                    RaiseLum();}
+                System.out.print("Luminosity Regulated");
+                return true; 
+            }
+        }
+        return false;
+    }
+    
+    public boolean Darken(){
+        if (isOn()==true){
+            if (CurrentLux>DesiredLux){
+                while(CurrentLux<DesiredLux){
+                    LowerLum();}
+                System.out.println("Luminosity Regulated");
+                return true; 
+            }
+        }
+        return false;
+    
+    }
+    
+    public boolean RaiseLum(){
+    
+    }
+    public boolean LowerLum(){
+    
+    }
+    public boolean turnOn(){
+        if (isOn()==true){
+           System.out.println("Light already on");
+           return false;
+        } 
+        setSwitch(true);
+        return true;
+        
+    }
+    
+    public boolean turnOff(){
+        if (isOn()==false){
+           System.out.println("Light already off");
+           return false;
+        } 
+        setSwitch(false);
+        return true;
+    }
+    
+    
+    
 }
